f236c8cf13e2639aebd691c4d8c1e7838804d4a4
Edson Reis <83223751+edsonsreis@users.noreply.github.com>
2025-02-21 16:45:07 -0300

    Atualização de Itens parte 1

--- scripts.js
+++ scripts.js
@@ -14,3 +14,35 @@ $(document).ready(function () {
         return false;
     });
 });
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const directorIcon = document.getElementById('directorIcon');
+    const directorAudio = document.getElementById('directorAudio');
+    const playlistIcon = document.getElementById('playlistIcon');
+    const playlistButton = document.getElementById('playlistButton');
+    const audioList = document.getElementById('audioList');
+
+    // Toggle da lista de áudio ao clicar no ícone da playlist
+    playlistIcon.addEventListener('click', () => {
+        audioList.classList.toggle('show');
+    });
+
+    // Exibir/ocultar lista ao clicar no botão Playlist
+    playlistButton.addEventListener('click', () => {
+        audioList.classList.toggle('show');
+    });
+
+    // Fechar a lista e parar qualquer áudio quando clicar fora da área da playlist
+    document.addEventListener('click', (event) => {
+        if (!playlistIcon.contains(event.target) && !playlistButton.contains(event.target) && !audioList.contains(event.target)) {
+            audioList.classList.remove('show');
+            stopAllAudio();
+        }
+    });
+
+
+
+});
